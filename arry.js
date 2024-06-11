ce49b52076d3958f44e37c2cfb4111d180fb95b3
spacecat04 <giovanna.mariot@escola.pr.gov.br>
2024-06-11 10:55:41 -0300

    tarefa

---
+++ arry.js
@@ -0,0 +1,15 @@
+const lista = [1, 2, 3, 4, 5];
+lista.pop();
+console.log(lista);
+
+const lista2 = [1, 2, 3, 4, 5];
+const lista5 = lista2.slice(1);
+console.log(lista5);
+
+const lista3 = [1, 2, 3, 4, 5];
+lista3.push(6);
+console.log(lista3);
+
+const lista4 = [1, 2, 3, 4, 5];
+lista4.splice(2,2,10);
+console.log(lista4);
