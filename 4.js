d442b04819fffe5415d7599fb48f4ff1ec14ef1f
spacecat04 <giovanna.mariot@escola.pr.gov.br>
2024-06-13 11:53:07 -0300

    p2

---
+++ 4.js
@@ -0,0 +1,6 @@
+const arrayoriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayco = arrayoriginal.concat("andré", "Fernanda")
+console.log(arrayoriginal)
+console.log(arrayco)
+const arrayoriginal_2 = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayco2 = arrayoriginal_2.concat(["andré"])
