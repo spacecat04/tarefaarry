33e0c664761731ccccc4af652cc5b22257ae3d67
spacecat04 <giovanna.mariot@escola.pr.gov.br>
2024-06-18 11:05:55 -0300

    18/

---
+++ 6.js
@@ -0,0 +1,14 @@
+let numeros =[5.5, 6.7, 7.9, 9.8]
+let soma = 0;
+let contagem = 0;
+for (let numero of numeros){
+    soma += numero;
+}
+let media = soma / numeros.length;
+console.log(media);
+
+
+////////////////////////////////////
+let nota = [10, 20, 30, 40, 50];
+let soma2 = 0;
+for (let i )
